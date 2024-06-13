26783583f17f2ea82a57dcb84dde54a9f87f39b9
tales-of-zuna <tales.of.zuna@gmail.com>
2024-06-13 13:27:56 +0800

    not sure

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -8,16 +8,27 @@ const Notfound = () => {
     <main className="flex h-screen flex-col items-center justify-center space-y-4">
       <p className="text-8xl font-bold">404</p>
       <div className="h-1 w-32 rounded-xl bg-neutral-700"></div>
-      <p className="text-3xl font-light">Page not found</p>
-      <Button
-        onClick={() => {
-          router.back();
-        }}
-        className="z-20 border text-white"
-        variant="bordered"
-      >
-        Back
-      </Button>
+      <p className="text-3xl font-light">Seems like you have lost</p>
+      <div className="flex items-center justify-center gap-2">
+        <Button
+          onClick={() => {
+            router.push("/");
+          }}
+          className="z-20 border text-white"
+          variant="bordered"
+        >
+          Home
+        </Button>
+        <Button
+          onClick={() => {
+            router.back();
+          }}
+          className="z-20 border text-white"
+          variant="bordered"
+        >
+          Back
+        </Button>
+      </div>
     </main>
   );
 };
