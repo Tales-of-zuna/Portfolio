ebb9a07be6fc1b1e2a479ae2d962431294b003a9
tales-of-zuna <tales.of.zuna@gmail.com>
2024-07-29 18:07:19 +0800

    link fixes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -130,7 +130,7 @@ const Home = () => {
           out some of my photography work on my{" "}
           <Link
             className="font-semibold text-gray-200 hover:underline"
-            href={"https://www.instagram.com/zuna.btw/?hl=en"}
+            href={"https://www.instagram.com/zuna.btw"}
           >
             {` Instagram `}
           </Link>{" "}
