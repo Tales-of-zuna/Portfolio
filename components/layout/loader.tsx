bab1fac48d7e6164adbcc81c45bd8ffab91206be
tales-of-zuna <tales.of.zuna@gmail.com>
2024-07-30 10:44:43 +0800

    loading animation update

--- components/layout/loader.tsx
+++ components/layout/loader.tsx
@@ -1,6 +1,14 @@
 import "@/styles/loader.css";
+import { Player } from "@lottiefiles/react-lottie-player";
 const Loader = () => {
-  return <div className="loader" />;
+  return (
+    <Player
+      autoplay
+      loop
+      src="/assets/animations/loader.json"
+      className="h-60 w-60"
+    ></Player>
+  );
 };
 
 export default Loader;
